9b878e2c11b4b2ad4e5d415fcdf9fdec4401d617
Shakirov <d31m07y1988@gmail.com>
2016-12-11 11:14:13 +0300

    класс для хранения записи

--- src/main/java/ru/otr/addressBook/Record.java
+++ src/main/java/ru/otr/addressBook/Record.java
@@ -1,7 +1,38 @@
 package ru.otr.addressBook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Ramil on 11.12.2016.
  */
 public class Record {
+
+    private String name;
+    private List<String> phones = new ArrayList<>();
+
+    public Record(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getPhones() {
+        return phones;
+    }
+
+    public void addPhone(String phoneNumber) {
+        phones.add(phoneNumber);
+    }
+
+    @Override
+    public String toString() {
+        return name + ": phones:" + phones;
+    }
 }
