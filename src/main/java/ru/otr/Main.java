7256df494020c2dcba9e978479208a2f2254d5fa
Shakirov <d31m07y1988@gmail.com>
2016-12-11 12:01:54 +0300

    класс запуска приложения

--- src/main/java/ru/otr/Main.java
+++ src/main/java/ru/otr/Main.java
@@ -1,10 +1,25 @@
 package ru.otr;
 
+import ru.otr.addressBook.Storage;
+
+import java.util.List;
+
 /**
  * Created by Ramil on 11.12.2016.
  */
 public class Main {
     public static void main(String[] args) {
-
+        if (args.length == 1) {
+            Storage storage = new Storage();
+            List<String> phonesByName = storage.getPhonesByName(args[0]);
+            if (phonesByName == null) {
+                System.out.println("Запись не найдена");
+            } else {
+                int i = 1;
+                for (String phone : phonesByName) {
+                    System.out.println(i++ + ". " + phone);
+                }
+            }
+        }
     }
 }
